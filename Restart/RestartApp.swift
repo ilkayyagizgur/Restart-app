0c4249f4e0f327ad8e0c4778230c0ef721a8366b
İlkay Yağız Gür <gurilkayyagiz@gmail.com>
2022-12-15 16:07:35 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Ada Yagiz Gur on 8.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
